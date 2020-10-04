515a5aac233432c2054c412c5cb10b2b5960c6c8
ChrystopherBorba <35618636+ChrystopherBorba@users.noreply.github.com>
2020-10-04 20:36:12 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using eugenio.Models;
+
+namespace eugenio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Desconto()
+        {
+            return View();
+
+        }
+        public ActionResult GeranoDesconto()
+        {
+            return View();
+        }
+        public ActionResult Produtos()
+        {
+            ProdutoModel model = new ProdutoModel();
+            Produto produto = new Produto();
+            List<Produto> lista = model.Consultarproduto("9781305399099");
+            return View(lista);
+        
+        }
+        public ActionResult Produtos2()
+        {
+            return View();
+        }
+        public ActionResult Leitor()
+        {
+           return View();
+           
+        }
+        public ActionResult teste()
+        {
+            return View();
+
+        }
+
+
+
+    }
+}
---
+++ Models/ProdutoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using eugenio.Models;
+
+    public class ProdutoModel 
+    {
+        public List<Produto> Consultarproduto(string barcode)
+        {
+            List<Produto> lista = new List<Produto>();
+
+            string conexao = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\eugenio_web_rev0f\\eugenio_web\\eugenio\\App_Data\\Natura.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection cn = new SqlConnection(conexao);
+            SqlCommand cmd = new SqlCommand($"SELECT * FROM Produto WHERE barcode = {barcode}", cn);
+            cn.Open();
+              
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Produto produto = new Produto();
+                produto.Name = (string)reader["name"];
+                produto.Type = (string)reader["type"];
+                lista.Add(produto);
+            }
+            return lista;
+
+        }
+    }
