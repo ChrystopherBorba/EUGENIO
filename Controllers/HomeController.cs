78930644e60912a01fb42c72d9cbca9b85d97e0e
ChrystopherBorba <35618636+ChrystopherBorba@users.noreply.github.com>
2020-10-04 20:37:38 -0300

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace eugenio.Controllers
         {
             ProdutoModel model = new ProdutoModel();
             Produto produto = new Produto();
-            List<Produto> lista = model.Consultarproduto("9781305399099");
+            List<Produto> lista = model.Consultarproduto(Produto.BarCode);
             return View(lista);
         
         }
